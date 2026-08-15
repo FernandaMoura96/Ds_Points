9a186ea693f668f274401e4bffcb96f122ad3c21
Fernanda Moura <ferferreira199609@gmail.com>
2026-08-15 19:44:12 -0300

    df report_metrics

--- src/train/train.py
+++ src/train/train.py
@@ -6,6 +6,7 @@ import sqlalchemy
 from sklearn import model_selection 
 from sklearn import ensemble
 from sklearn import pipeline
+from sklearn import metrics
 
 from feature_engine import encoding
 
@@ -62,5 +63,32 @@ model = ensemble.RandomForestClassifier(random_state=42)
 model_pipeline = pipeline.Pipeline([("One Hot Encode", onehot),
                                     ("Modelo",model)])
 # %%
+#Ajuste de modelo 
+
 model_pipeline.fit(X_train, y_train)
-# %%
+
+#Aplicação do modelo para diferentes bases de dados 
+y_train_proba = model_pipeline.predict_proba(X_train )
+y_test_proba = model_pipeline.predict_proba(X_test)
+y_oot_proba = model_pipeline.predict_proba(df_oot[features])
+
+def report_metrics(y_true, y_proba, cohort=0.5):
+
+    y_pred = (y_proba[:,1]>cohort).astype(int)
+
+    acc = metrics.accuracy_score(y_true, y_pred)
+    auc = metrics.roc_auc_score(y_true, y_proba[:,1])
+    precision = metrics.precision_score(y_true, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+
+    res = {
+        'Acurárica': acc,
+        'Curva Roc': auc,
+        "Precisão": precision,
+        "Recall": recall,
+        }
+
+    return res
+report_train = report_metrics(y_train,y_train_proba)
+report_test = report_metrics(y_test,y_test_proba)
+report_oot = report_metrics(df_oot[target],y_oot_proba)
