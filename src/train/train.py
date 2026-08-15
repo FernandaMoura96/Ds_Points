5ccab834cd63c6b230dd7b5d72c121a5190ed4d4
Fernanda Moura <ferferreira199609@gmail.com>
2026-08-15 19:18:27 -0300

    Definindo modelo de pipeline

--- src/train/train.py
+++ src/train/train.py
@@ -1,9 +1,13 @@
 # %%
 
-
 import pandas as pd
 import sqlalchemy
 
+from sklearn import model_selection 
+from sklearn import ensemble
+from sklearn import pipeline
+
+from feature_engine import encoding
 
 # %%
 
@@ -30,5 +34,33 @@ target = 'flChurn'
 features = df_train.columns[3:].tolist()
 
 # %%
+X_train, X_test, y_train, y_test = model_selection.train_test_split(df_train[features],
+                                                                    df_train[target],
+                                                                    random_state= 42,
+                                                                    train_size= 0.8)
 
-[]
+print('Taxa de resposta na base train', y_train.mean())
+print('Taxa de resposta na base test', y_test.mean())
+# %%
+cat_features = X_train.dtypes[X_train.dtypes == 'object'].index.tolist()
+num_features = list(set(features) - set(cat_features))
+num_features
+
+# %%
+X_train[cat_features].describe()
+
+X_train[cat_features].drop_duplicates()
+# %%
+X_train[num_features].describe().T
+# %%
+X_train[num_features].isna().sum().max()
+# %%
+onehot = encoding.OneHotEncoder(variables =cat_features,
+                                drop_last=True)
+model = ensemble.RandomForestClassifier(random_state=42)
+
+model_pipeline = pipeline.Pipeline([("One Hot Encode", onehot),
+                                    ("Modelo",model)])
+# %%
+model_pipeline.fit(X_train, y_train)
+# %%
