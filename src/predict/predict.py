86422e984f9feb7e7e74d487b8801082b13c8d7d
Fernanda Moura <ferferreira199609@gmail.com>
2026-08-17 20:43:28 -0300

    Finalizando ajustes no script de predict

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -4,3 +4,7 @@ import pandas as pd
 
 model_series = pd.read_pickle("../../models/rf_fim_de_curso.pkl")
 # %%
+
+engine =  sqlalke
+with open ("etl.sql",'r')  as open_file:
+    query = open_file.read()
