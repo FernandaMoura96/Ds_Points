7819b36ec4bb35c713b4486d7c99d2b37173ad98
Fernanda Moura <ferferreira199609@gmail.com>
2026-08-24 20:55:33 -0300

    Probablidade de churn

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -1,10 +1,30 @@
 # %% 
-
 import pandas as pd
+import sqlalchemy
 
-model_series = pd.read_pickle("../../models/rf_fim_de_curso.pkl")
-# %%
+# 1. Carrega o modelo treinado (certifique-se de que o nome do arquivo bate com o do treino)
+model_series = pd.read_pickle("../../models/rf_fim_curso.pkl")
 
-engine =  sqlalke
-with open ("etl.sql",'r')  as open_file:
+# 2. Conexão com o banco de dados
+engine = sqlalchemy.create_engine("sqlite:///../../data/feature_store.db")
+
+# 3. Leitura correta do arquivo SQL (com os parênteses no .read())
+with open("etl.sql", "r") as open_file:
     query = open_file.read()
+
+# 4. Carrega os dados da base mais recente através da query
+df = pd.read_sql(query, engine)
+
+# %%
+# 5. Extrai o pipeline e a lista correta de features salvas no modelo
+pipeline_modelo = model_series["model"]
+features_modelo = model_series["features"]
+
+# 6. Aplica a predição de probabilidade usando apenas as colunas treinadas
+pred = pipeline_modelo.predict_proba(df[features_modelo])
+
+# Exibe o resultado das probabilidades
+print(pred)
+
+
+# %%
