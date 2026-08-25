8e4d7ba8cd86983a3f11fe9c111c8395012b98e7
Fernanda Moura <ferferreira199609@gmail.com>
2026-08-24 21:06:21 -0300

    Predict into

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -1,30 +1,30 @@
-# %% 
+#%%
 import pandas as pd
 import sqlalchemy
 
-# 1. Carrega o modelo treinado (certifique-se de que o nome do arquivo bate com o do treino)
-model_series = pd.read_pickle("../../models/rf_fim_curso.pkl")
 
-# 2. Conexão com o banco de dados
+
+model_series = pd.read_pickle("../../models/rf_fim_curso.pkl")
+model_series
+
+#%%
+
 engine = sqlalchemy.create_engine("sqlite:///../../data/feature_store.db")
 
-# 3. Leitura correta do arquivo SQL (com os parênteses no .read())
-with open("etl.sql", "r") as open_file:
+# ATENÇÃO AQUI: Adicionado o encoding='utf-8'
+with open("etl.sql", 'r', encoding='utf-8') as open_file:
     query = open_file.read()
 
-# 4. Carrega os dados da base mais recente através da query
+# Carrega os dados
 df = pd.read_sql(query, engine)
+#%%
+# Faz a predição
+pred = model_series['model'].predict_proba(df[model_series['features']])
+proba_churn = pred[:, 1]
 
+df_predict = df[['idCustomer']].copy()
+df_predict['prob_churn'] = proba_churn
+df_predict = (df_predict.sort_values("prob_churn", ascending=False))
 # %%
-# 5. Extrai o pipeline e a lista correta de features salvas no modelo
-pipeline_modelo = model_series["model"]
-features_modelo = model_series["features"]
-
-# 6. Aplica a predição de probabilidade usando apenas as colunas treinadas
-pred = pipeline_modelo.predict_proba(df[features_modelo])
-
-# Exibe o resultado das probabilidades
-print(pred)
-
-
+df_predict
 # %%
